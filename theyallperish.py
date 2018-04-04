bcde2485913b3730ce74039050d2678af04e8337
sd4483 <sudheer.paturi@hotmail.com>
2018-04-05 01:36:44 +0800

    Completed building a basic model
    This is just a basic model with taking into account only the numeric dataa types. Have to optimize and do feature engineering to make a better model.

--- theyallperish.py
+++ theyallperish.py
@@ -13,29 +13,32 @@ import pandas as pd
 # Importing the dataset
 train_dataset = pd.read_csv('train.csv')
 test_dataset = pd.read_csv('test.csv')
-X_train = train_dataset.iloc[:, [2,4,5,6,7,9,11]].values
-y_train = train_dataset.iloc[:, 1].values
-X_test = test_dataset.iloc[:,[1,3,4,5,6,8,10]].values
+X = train_dataset.iloc[:,[2,4,5,6,7,9]]
+y = train_dataset.iloc[:,1]
+test = test_dataset.iloc[:,[1,3,4,5,6,8]]
 
-#Missing Values
-from sklearn.preprocessing import Imputer                               
-imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)  
-imputer = imputer.fit(X_train[:,2])                                       
-X_train[:,2] = imputer.transform(X_train[:,2])                               
+
+# Missing Data [Training Set]
+X_missing = X.isnull()
+X_missing_sum = X.isnull().sum()
+avg_Age = X.Age.mean()
+X.Age = X.Age.fillna(value = avg_Age)
+X_missing_dealtwith = X.isnull().sum()
 
 
 #Encoding Categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder           
-labelencoder_country = LabelEncoder()                                   
-X[:,0] = labelencoder_country.fit_transform(X[:,0])
-onehotencoder = OneHotEncoder(categorical_features = [0])               
-X = onehotencoder.fit_transform(X).toarray()
+labelencoder_sex = LabelEncoder()                                   
+X.Sex = labelencoder_sex.fit_transform(X.Sex)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Fitting classifier to the Training set
 from sklearn.tree import DecisionTreeClassifier
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, y_train)
-
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -43,3 +46,23 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+
+# PREPROCESSING TEST SET
+
+# Missing Data [Test Set]
+test_missing = test.isnull()
+test_missing_sum = test.isnull().sum()
+avg_Age = test.Age.mean()
+test.Age = test.Age.fillna(value = avg_Age)
+avg_Fare = test.Fare.mean()
+test.Fare = test.Fare.fillna(value = avg_Fare)
+test_missing_dealtwith = test.isnull().sum()
+
+
+#Encoding Categorical data for test set        
+labelencoder_testdata_sex = LabelEncoder()                                   
+test.Sex = labelencoder_testdata_sex.fit_transform(test.Sex)
+
+# Predicting the model on real test set results
+Test_pred = classifier.predict(test)
